e9080d6dc3a09dcc5c5f56dedfa6c9748fd8eafd
= <=>
2023-08-01 12:23:59 +0200

    Add supabase and getCabins files into services

---
+++ src/services/supabase.js
@@ -0,0 +1,14 @@
+/**
+ * RVr4YM4RuJAExfB5
+  Geraldina, Adrian, Flor, Andrés
+ * 
+ */
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://gltgifhgwfhtxdvsysvj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsdGdpZmhnd2ZodHhkdnN5c3ZqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA4MjgxNjksImV4cCI6MjAwNjQwNDE2OX0.v5tEvY_Zx33quPeA6O309NiRVPs4IFnjvMpohYBJ9Bg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
